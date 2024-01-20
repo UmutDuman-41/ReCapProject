d5fdca899f55e80ad042e7ca196c69d90a810e12
UMUT <Uduman0629@gmail.com>
2024-01-20 23:54:41 +0300

    Eklemeler

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
+        
+        void Add(T Entity);
+        void Delete(T Entity);
+        void Update(T Entity);
+    }
+}
