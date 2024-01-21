1f19aa84c09b6e89634a6ed6cfd465e21a789682
UMUT <Uduman0629@gmail.com>
2024-01-21 18:26:30 +0300

    Ekleme

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,17 +9,28 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            CarManager carManager1 = new CarManager(new EfCarDal());
+            foreach (var manager in carManager1.GetCarsDailyPrice())
+            {
+                Console.WriteLine(manager.Description);
+            }
+            Car car = new Car();
+            car.CarId = 1;
+            car.CarName = "Test";
+            car.BrandId = 1;
+            car.ColorId = 1;
+            car.ModelYear = "1999";
+            car.DailyPrice = 500;
+            car.Description = "Test model";
+
+            carManager1.Add(car);
             CarManager carManager = new CarManager(new InMemoryCarDal());
             foreach (var manager in carManager.GetAll())
             {
                 Console.WriteLine(manager.Description);
             }
 
-            CarManager carManager1 = new CarManager(new EfCarDal());
-            foreach (var manager in carManager1.GetCarsDailyPrice())
-            {
-                Console.WriteLine(manager.Description);
-            }
+          
 
 
 
